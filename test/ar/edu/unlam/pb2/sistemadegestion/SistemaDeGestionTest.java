e29ed41023b809a3a6d5ac980aecc39c916d7bb3
Eros Panuccio <erospanuccio@gmail.com>
2025-11-18 20:12:35 -0300

    Testeando ando

---
+++ test/ar/edu/unlam/pb2/sistemadegestion/SistemaDeGestionTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2.sistemadegestion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SistemaDeGestionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
